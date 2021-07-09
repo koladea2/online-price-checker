0b060671483c202ee05c9274037b3dcdc984fd95
Juan SBrinez <juansebri2001@gmail.com>
2021-07-09 17:04:15 +0000

    Second Complete Update

--- testFile.py
+++ testFile.py
@@ -6,14 +6,11 @@ class TestFileName(unittest.TestCase):
     def test_userinput_not_empty(self):
         self.assertNotEqual(game_difficulty(), "")
 
-
     def test_userinput_difficulty(self):
         self.assertEqual(game_difficulty(), "hard" or "medium" or "easy")
 
-
     def test_userinput_negative(self):
         self.assertNotEqual(game_difficulty(), "-1")
-
 
     def test_userinput_greaterthan2(self):
         self.assertNotEqual(game_difficulty(), "3")
@@ -23,34 +20,26 @@ class TestFileName(unittest.TestCase):
 
 #       def test_printquestions_string(self):
 #           self.assertNotEqual(print_questions(Dic), "")
-
     def test_displayChoice_isLess(self):
         self.assertLess(display_ids(), "33")
-
 
     def test_displayChoice_isGreater(self):
         self.assertGreater(display_ids(), "8")
 
-
     def test_displayChoice_isGreaterEqual(self):
         self.assertGreater(display_ids(), "9")
-
 
     def test_displayChoice_isLess(self):
         self.assertLessEqual(display_ids(), "32")
 
-
     def test_displayIdchoice_isnot_string(self):
         self.assertNotEqual(display_ids(), "entertainment")
-
 
     def test_display_ids_stringRep(self):
         self.assertNotEqual(display_ids(), "Art")
 
-
     def test_introduction_notInt(self):
         self.assertNotEqual(introduction(), 9)
-
 
 if __name__ == '__main__':
     unittest.main()
